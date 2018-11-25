ac927fd1ee5f0080e93c35529ffc0f7e703d8d03
daniellectlr <daniellectlr@gmail.com>
2018-11-25 15:01:51 +0000

    Add files via upload

--- simulateddata_case/loglik.R
+++ simulateddata_case/loglik.R
@@ -1,34 +1,37 @@
 loglik <- function(THETA,data){
   
+  # bird movement data
   all.tracks <- data$tracks
   SPS <- all.tracks[,1]
-  corridor <- data$corridor
-  patch <- data$patch
-  di <- data$di
-  
   SP  <- data$SP
   ns <- length(SP)
   
-  like<-rep(0,ns)
-  for (k in 1:ns) {
-    theta = THETA[k, ]
-    tracks = all.tracks[SPS == SP[k],]
+  #environmental covariates
+  semiopen <- data$semiopen
+  forest <- data$forest
+  d <- data$d
+  
+  like<-rep(0, ns)
+  
+  for (k in 1: ns) {
+    theta <- THETA[k, ]
+    tracks <- all.tracks[SPS == SP[k],]
     
     alpha <- exp(theta[1])
-    betaC   <- theta[2]
-    betaP   <- theta[3]
+    beta_s   <- theta[2]
+    beta_f   <- theta[3]
     li    <- 0
     
-    idt <- tracks[,2]
+    idt <- tracks[ ,2]
     uid <- unique(idt)
-    ta <- tracks[,3]
+    ta <- tracks[ ,3]
     
-    for(ss in 1:length(uid)){ #for each one of the tracks
-      lta <- ta[idt==uid[ss]] #data for the focal track
+    for(ss in 1: length(uid)){ #for each one of the tracks
+      lta <- ta[idt == uid[ss]] #data for the focal track
       
-      for(i in 1:(length(lta)-1)){ #for each movement step
+      for(i in 1: (length(lta)-1)) { #for each movement step
         x <- lta[i]
-        pr <- exp(-di[x,]/alpha) * exp(betaC * corridor) * exp(betaP * patch)
+        pr <- exp(-d[x,]/alpha) * exp(beta_s * semiopen) * exp(beta_f * forest)
         pr <- pr / sum(pr)
         pr <- pr + 10^(-10)
         pr <- pr / sum(pr)
